fddb2e8b1ec582837231d53d526a5365da7fec42
Argha-Majumder <majumderargha847@gmail.com>
2023-09-19 00:47:26 +0530

    made some changes

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ var slider = document.getElementById("slider");
 var arrowLeft = document.getElementById("arrow-left");
 var arrowRight = document.getElementById("arrow-right");
 
+let contentListOne = document.getElementById('content1');
+let contentListTwo = document.getElementById('content2');
+let toggleOne = true;
+let toggleTwo = true;
+
 arrowLeft.addEventListener("click", function() {
-  slider.scrollLeft -= slider.offsetWidth;
+    slider.scrollLeft -= slider.offsetWidth;
 });
 
 arrowRight.addEventListener("click", function() {
-  slider.scrollLeft += slider.offsetWidth;
+    slider.scrollLeft += slider.offsetWidth;
 });
+
+function openMenuOne() {
+    if (toggleOne) {
+        toggleTwo = false;
+        openMenuTwo();
+    }
+    contentListOne.style.visibility = toggleOne?'visible':'hidden';
+    toggleOne = !toggleOne;
+}
+
+function openMenuTwo() {
+    if (toggleTwo) {
+        toggleOne = false;
+        openMenuOne();
+    }
+    contentListTwo.style.visibility = toggleTwo?'visible':'hidden';
+    toggleTwo = !toggleTwo;
+}
+
